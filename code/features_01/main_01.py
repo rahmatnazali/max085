cc6e9accd3f8cdfe4f63067e78aac3a2a5e259ad
rahmat <rahmatnazali95@gmail.com>
2019-03-25 21:26:05 +0700

    update main code

--- code/features_01/main_01.py
+++ code/features_01/main_01.py
@@ -13,38 +13,52 @@ if not url_list:
     print("URLS.txt not found.")
     exit(1)
 
-list_data = []
-regex_instance_list = lib.compile_regex(config.Regex)
-list_attribute = lib.regex_xpath_to_attribute(config.XPath)
+
+
+# todo: populate link_list with href from Done.csv
+link_list = lib.read_done_csv() # each link from Done.csv AND links found from current scraps will be stored here. It is guaranteed that no duplicate link exist here
+
+list_data = [] # list of row data to be converted to CSV later
+
+regex_instance_list = lib.compile_regex(config.Regex) # compiled regex
+
+list_attribute = lib.regex_xpath_to_attribute(config.XPath) # list of attributes
 
 
 
 for url in enumerate(url_list):
     print("Scraping URL ({}/{}) from URLs.txt Using XPath ...".format(url[0] + 1, len((url_list))))
 
-    # todo: request the url
+    # get url page. commented for now for testing purpose.
     # html_page = lib.get_page(url)
 
-    # todo: scrap for any links
+    # for testing purpose
     with open("example.html") as html_file:
         html_page = html_file.read()
 
-    # print(html_page)
-    links_found = lib.scrap_html(html_page)
+    # scrap the html based on its xpath
+    links_found = lib.scrap_html(html_page, config.XPath)
 
 
     if links_found:
-        # todo: CSV Operation. store title and the link on csv
-        # todo IMPORTANT: it should be able to get the @attribute, maybe should have been built using class
-
         print("\tAdding Results to Output.csv ...  ({} link(s) found)".format(len(links_found)))
         for link in links_found:
 
             a_row = {}
             for attribute in list_attribute:
-                a_row[attribute] = link.get(attribute, '')
+                an_attribute = link.get(attribute, '')
+                regexed_attribute = lib.regex_string(an_attribute, regex_instance_list)
+                a_row[attribute] = regexed_attribute
 
-            list_data.append(a_row)
+            """
+            An occurrence is evaluated here.
+            If href is in link_list, then ignored it.
+            If href is not in link_list, add it
+            """
+            # print("Regex Search & Replace in Output.csv ...")
+            if a_row['href'] not in link_list:
+                link_list.append(a_row['href'])
+                list_data.append(a_row)
 
         for i in list_data:
             print(i)
@@ -62,28 +76,19 @@ for url in enumerate(url_list):
     # todo: might be better to evalate all the given condition before saving the csv. e.g. better to regex it first, then remove multiple occurence, and finaly save it. will be much efficient.
     # insert
 
-
-# todo
+# [DONE] Regex is already done in iteration. This will be more efficient
 print("Regex Search & Replace in Output.csv ...")
-# make a function that if called, will iterate each row and re.sub the title and links
-for row in list_data:
-    for attribute in list_attribute:
-        row[attribute] = lib.regex_string(row[attribute], regex_instance_list)
 
-# at this point, all the element should be replaced with regex. to check it, run the code below
-for row in list_data:
-    print(row)
-
-exit()
+# to check, at this point all the element should be replaced with regex at iteration, run the code below
+# for row in list_data:
+#     print(row)
 
 
-# todo
+# [DONE] multiple occurrence in Rows is already evaluated in iteration. This will be more efficient
 print("Removing Rows In Output.csv If The Row Also In Done.csv ...")
-# check if Done.csv is there
-# if yes, read it and evaluate the occurrence. remove the duplicate of list_data, and add(append) the list_data to Done.scv
-# if no, create empty Done.csv
 
-# todo
+
+# todo: should evaluate config.column
 print("Removing Rows With Same Column # Values Except 1st Occurrence In Output.csv ...")
 
 
