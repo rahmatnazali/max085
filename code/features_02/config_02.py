3602f86aa673f56783622920b0f4310bdb19828d
rahmat <rahmatnazali95@gmail.com>
2019-04-01 19:02:47 +0700

    add user agent for request header

--- code/features_02/config_02.py
+++ code/features_02/config_02.py
@@ -23,6 +23,11 @@ UseProxies              = False
 ShowBrowserWindows      = True or False
 
 
+# request header for requests module
+RequestHeader = {
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
 """
 Credentials Option
 """
